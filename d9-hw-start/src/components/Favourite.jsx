c6ba1c05bc57e6ba4d55a432fc13200d9ce3312f
Federico Poggi <federicopoggi3@gmail.com>
2023-11-06 16:35:17 +0100

    first

---
+++ d9-hw-start/src/components/Favourite.jsx
@@ -0,0 +1,8 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+
+const Favourite = ({ addedToFavouirite }) => {
+  const dispatch = useDispatch();
+
+  return;
+};
